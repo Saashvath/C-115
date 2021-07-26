e11fcd6eca4e258f4c023ca68b413dad31bce64f
Saashvath <74235537+Saashvath@users.noreply.github.com>
2021-07-26 17:49:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+}
+
+
+function setup(){
+
+
+ canvas = createCanvas(400,400)
+ canvas.position(625 ,40);
+}
+
+function draw(){
+
+}
+
+function take_picture(){
+
+    save("C-115.png")
+}
